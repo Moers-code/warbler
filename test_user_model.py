8c8f4df1cda44f3e3510e9c9027249d4e4912597
Moers <shaaban.moe@gmail.com>
2023-05-09 21:46:46 +0200

    add test_user_model

--- test_user_model.py
+++ test_user_model.py
@@ -8,7 +8,7 @@
 import os
 from unittest import TestCase
 
-from models import db, User, Message, Follows
+from models import db, User, Message, Follows, bcrypt
 
 # BEFORE we import our app, let's set an environmental variable
 # to use a different database for tests (we need to do this
@@ -26,33 +26,61 @@ from app import app
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
 
-db.create_all()
-
 
 class UserModelTestCase(TestCase):
-    """Test views for messages."""
-
     def setUp(self):
-        """Create test client, add sample data."""
+        """Creates a new database for the unit test to use."""
+        with app.app_context():
+            db.create_all()
 
-        User.query.delete()
-        Message.query.delete()
-        Follows.query.delete()
+    def tearDown(self):
+        """Closes the database again at the end of the test."""
+        with app.app_context():
+            db.session.remove()
+            db.drop_all()
+            
 
-        self.client = app.test_client()
+    def test_user_password_hashing(self):
+        with app.app_context():
+            u = User.signup(username='test', password='password', email='test@example.com', image_url='')
+            
+            self.assertFalse(User.authenticate('test','not-password'))
+            self.assertTrue(User.authenticate('test', 'password'))
 
-    def test_user_model(self):
-        """Does basic model work?"""
+    def test_user_repr(self):
+        with app.app_context():
+            u = User.signup(username='test', password='password', email='test@example.com', image_url='')
+            
+            db.session.commit()
+            self.assertEqual(str(u), '<User #1: test, test@example.com>')
 
-        u = User(
-            email="test@test.com",
-            username="testuser",
-            password="HASHED_PASSWORD"
-        )
+    def test_user_signup(self):
+        with app.app_context():
+            u = User.signup(username='test', password='password', email='test@example.com', image_url='')
+            db.session.commit()
+            self.assertEqual(u.username, 'test')
+            self.assertEqual(u.email, 'test@example.com')
+            
 
-        db.session.add(u)
-        db.session.commit()
+    def test_user_authenticate(self):
+        with app.app_context():
+            u = User.signup(username='test', password='password', email='test@example.com', image_url='')
+            db.session.commit()
+            user = User.authenticate('test', 'password')
+            self.assertTrue(user)
+            self.assertEqual(user.username, 'test')
+            self.assertEqual(user.email, 'test@example.com')
+            self.assertFalse(User.authenticate('test', 'not-password'))
 
-        # User should have no messages & no followers
-        self.assertEqual(len(u.messages), 0)
-        self.assertEqual(len(u.followers), 0)
+    def test_user_following(self):
+        with app.app_context():
+            u1 = User.signup(username='test', password='password', email='test@example.com', image_url='')
+            u2 = User.signup(username='test2', password='password', email='test2@example.com', image_url='')
+            
+            db.session.commit()
+            self.assertFalse(u1.is_following(u2))
+            u1.following.append(u2)
+            db.session.commit()
+            self.assertTrue(u1.is_following(u2))
+            self.assertTrue(u2.is_followed_by(u1))
+
